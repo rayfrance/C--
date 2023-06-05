2a60887db28cc76a3769537ffaa9595571757df0
rayfrance <rayanethaisglfrance@gmail.com>
2023-06-05 01:02:35 -0300

    implenta função letra_existe

--- jogodaforca.cpp
+++ jogodaforca.cpp
@@ -2,7 +2,41 @@
 #include <string>
 using namespace std;
 
+const string PALAVRA_SECRETA = "melancia";
+
+bool letra_existe(char chute){
+    for (int i = 0; i < PALAVRA_SECRETA.size(); i++)
+    {
+        if (chute == PALAVRA_SECRETA[i])
+        {
+            return true;
+        }
+    }
+    return false;
+    
+}
+
 int main () {
-    string palavra_secreta = "MELANCIA";
-    cout << palavra_secreta << endl;
+    cout << PALAVRA_SECRETA << endl;
+
+    bool nao_acertou = true;
+    bool nao_enforcou = true;
+
+    while (nao_acertou && nao_enforcou)
+    {
+        char chute;
+        cin >> chute;
+
+        if (letra_existe(chute))
+        {
+            cout << "Voce acertou! Seu chute esta na palavra" << endl;
+        }
+        else
+        {
+            cout << "Voce errou! Seu chute nao esta na palavra" << endl;
+        }
+        
+        
+    }
+    
 }
